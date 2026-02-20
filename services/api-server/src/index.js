38ee7d164a92eb62c971083044879f776f67dceb
Rishikesh-Reddy-Ch <cherukurishi95@gmail.com>
2026-02-20 18:46:57 +0530

    Made gateway listen to request over the network

--- services/api-server/src/index.js
+++ services/api-server/src/index.js
@@ -2,6 +2,6 @@ import { server } from "./app.js";
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`API Gateway (and WebSocket) running on port ${PORT}`);
 });
